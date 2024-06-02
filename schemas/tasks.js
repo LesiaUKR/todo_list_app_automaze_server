51b0f82b63b3867f87c8e9c5c353b2fe8914e18e
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-06-02 16:00:19 +0300

    fix: correct joi schema

--- schemas/tasks.js
+++ schemas/tasks.js
@@ -1,24 +1,28 @@
 const Joi = require("joi");
 
 const taskJoiSchema = Joi.object({
-  title: Joi.string().required().messages({
+  title: Joi.string().messages({
     "any.required": "Set task title",
   }),
-  description: Joi.string().required().messages({
+  description: Joi.string().messages({
     "any.required": "Set task description",
   }),
-  date: Joi.string().required().messages({
+  date: Joi.string().messages({
     "any.required": "Set task date",
   }),
-  completed: Joi.boolean().required().messages({
+  completed: Joi.boolean().messages({
     "any.required": "Set completed status",
   }),
-  priority: Joi.number().integer().min(1).max(10).required().messages({
+  priority: Joi.number().integer().min(1).max(10).messages({
     "any.required": "Set task priority",
     "number.min": "Priority must be at least 1",
     "number.max": "Priority must be at most 10",
   }),
 });
+
+module.exports = {
+  taskJoiSchema,
+};
 
 module.exports = {
   taskJoiSchema,
